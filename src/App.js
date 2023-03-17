e265098d74e328427440c4e3f5e18f330173fcdd
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-03-18 00:51:01 +0200

    Edit Dashboard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
+import { Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import * as authService from './services/authService.js';
+
 import Header from './components/Header/Header.js';
-import { Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard.js';
+import Register from './components/Register/Register.js';
+import Login from './components/Login/Login.js';
+import Details from './components/Details/Details.js';
+import Create from './components/Create/Create.js';
+import Edit from './components/Edit/Edit.js';
+import MyPets from './components/MyPets/MyPets.js';
+
 
 function App() {
+
+  const [userInfo, setUserInfo] = useState({ isAuth: false, username: '' });
+
+  useEffect(() => {
+    let user = authService.getUser();
+
+    setUserInfo({
+      isAuth: Boolean(user),
+      user
+    });
+
+  }, []);
+
+  const onLogin = (username) => {
+    setUserInfo({
+      isAuth: true,
+      user: username,
+    });
+  }
+
   return (
     <>
-      <Header />
+      <Header {...userInfo} />
 
       <main id="site-content">
-
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/login' element={<Login onLogin={onLogin} />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/details' element={<Details />} />
+          <Route path='/create' element={<Create />} />
+          <Route path='/edit' element={<Edit />} />
+          <Route path='/my-pets' element={<MyPets />} />
+        </Routes>
       </main>
 
       <footer id="site-footer">
