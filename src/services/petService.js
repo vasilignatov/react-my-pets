0749efaa5d1346fe33d5166f6b1d25dcea567667
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-03-20 17:20:44 +0200

    Add create pet request

--- src/services/petService.js
+++ src/services/petService.js
@@ -1,10 +1,28 @@
-const host = 'http://localhost:3030/jsonstore';
-
+const host = 'https://spa-practise-server.onrender.com/data/';
 export const getPets = async () => {
-    return (await fetch(host + '/my-pets')).json();
+    const res = await fetch(host + '/animals');
+    const data = await res.json();
+    
+    return Object.values(data);
 }
 
 export const getPetById = async (id) => {
-    return (await fetch(host + `/my-pets/${id}`)).json();
+    return (await fetch(host + `/animals/${id}`)).json();
 }
 
+export const createPet = async (pet) => {
+    return await fetch(host + `/animals/${pet._id}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(pet)
+    });
+}
+
+
+
+window.api = {
+    getPetById,
+    getPets
+}
