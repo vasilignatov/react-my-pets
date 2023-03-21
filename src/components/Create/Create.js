8936aae24113c3af0fcb85794a4becfb860f0361
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-03-21 17:04:12 +0200

    Create Pet functionality

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,29 +1,50 @@
+import { createPet } from '../../services/petService.js'
+import { useNavigate } from 'react-router-dom';
 const Create = () => {
+    const navigate = useNavigate();
+    
+    async function onSubmit(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+
+        const name = formData.get('name');
+        const description = formData.get('description');
+        const imageUrl = formData.get('imageUrl');
+        const type = formData.get('type');
+
+        createPet({ name, description, imageUrl, type })
+            .then(() => {
+                navigate('/dashboard');
+            });
+    }
+
+
     return (
         <section id="create-page" className="create">
-            <form id="create-form" action="" method="">
+
+            <form id="create-form" onSubmit={onSubmit}>
                 <fieldset>
                     <legend>Add new Pet</legend>
                     <p className="field">
-                        <label for="name">Name</label>
+                        <label htmlFor="name">Name</label>
                         <span className="input">
                             <input type="text" name="name" id="name" placeholder="Name" />
                         </span>
                     </p>
                     <p className="field">
-                        <label for="description">Description</label>
+                        <label htmlFor="description">Description</label>
                         <span className="input">
                             <textarea name="description" id="description" placeholder="Description"></textarea>
                         </span>
                     </p>
                     <p className="field">
-                        <label for="image">Image</label>
+                        <label htmlFor="image">Image</label>
                         <span className="input">
                             <input type="text" name="imageUrl" id="image" placeholder="Image" />
                         </span>
                     </p>
                     <p className="field">
-                        <label for="type">Type</label>
+                        <label htmlFor="type">Type</label>
                         <span className="input">
                             <select id="type" name="type">
                                 <option value="cat">Cat</option>
@@ -37,6 +58,7 @@ const Create = () => {
                     <input className="button submit" type="submit" value="Add Pet" />
                 </fieldset>
             </form>
+
         </section>
     )
 }
